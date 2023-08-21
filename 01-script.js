2327ad5d1d593ffebc63f98ec009ac9054ffa3d2
jonn <joliversalamanca@gmail.com>
2023-08-21 15:46:56 -0400

    primer commit

---
+++ 01-script.js
@@ -0,0 +1,9 @@
+const ob1 = {}
+ob1[0] = 'e0'
+ob1[1] = 'e1'
+ob1[2] = 'e2'
+ob1[3] = 'e3'
+ob1[4] = 'e4'
+console.log(ob1);
+const userAgent = navigator.userAgent.toLowerCase();
+console.log(userAgent);

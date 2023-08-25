17d8d49f19fabb0ec777ff60bc3555815fd5d484
yersin77 <pruebacorreoselectronicosjyos@gmail.com>
2023-08-25 11:04:08 -0400

    modifed: 01-script.js, 02-script.js, index.html, style.css

--- 01-script.js
+++ 01-script.js
@@ -1,5 +1,12 @@
-import { scroll } from "./02-script.js"
 
-document.addEventListener('DOMContentLoaded', e =>{
-    scroll()
+const $container = document.querySelector('.classContainer') 
+const  $div = document.createElement('div')
+$div.style.backgroundColor = 'red'
+$div.style.width = '50px'
+$div.style.height = '50px'
+
+const $allArticle = document.querySelectorAll('article')
+
+$allArticle.forEach(article =>{
+    console.log(article);
 })
--- 02-script.js
+++
@@ -1,5 +0,0 @@
-export function detectarDispositivo(){
-    const datos = navigator.userAgent.match(/windows/i)
-    console.log(datos)
-}
-
